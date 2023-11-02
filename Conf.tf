90a4fd2280361d0ec0592c6b534ef3dab5be3386
Bhargav <karkarebhargav08@gmail.com>
2023-11-02 17:49:19 +0530

    Teraform file

---
+++ Conf.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+resource "aws_instance" "TF_instance" {
+  ami = "ami-0763cf792771fe1bd"
+  instance_type = "t2.micro"
+  key_name = "mykey100"
+  vpc_security_group_ids = "sg-076de74cf330b6063"
+  tags = {
+    name = "new_instance"
+    env = "dev"
+  }
+}
