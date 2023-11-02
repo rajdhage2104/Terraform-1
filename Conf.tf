15a81a334d49e70cc8ad76031d595194bac02f70
Bhargav <karkarebhargav08@gmail.com>
2023-11-02 19:08:45 +0530

    Configuration file fully updated

--- Conf.tf
+++ Conf.tf
@@ -1,11 +1,64 @@
 provider "aws" {
   region = "ap-south-1"
 }
+#VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/20"
+  tags = {
+    name = My_vpc
+  }
+}
+#Subnet 
+resource "aws_subnet" "my_subnet" {
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true
+}
+#Security Group
+resource "aws_security_group" "my-sg" {
+  name = "sg1"
+  vpc_id = aws_vpc.my_vpc.id
+  ingress = {
+    from_port = 80
+    to_port = 80
+    protocol = "TCP"
+    cidr_block = ["0.0.0.0/0"]
+  } 
+  egress = {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_block = ["0.0.0.0/0"]
+  }
+}
+# Internet Gateway
+resource "aws_internet_gateway" "my-igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+# Route Table
+resource "aws_route_table" "my_route" {
+    vpc_id = aws_vpc.my_vpc.id
+}
+# Route to IGW
+resource "aws_route" "route_to_IGW" {
+  route_table_id = aws_route_table.my_route.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.my-igw.id
+}
+#Route to subnet
+resource "aws_route_table_association" "subnet_association" {
+  subnet_id = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route.id
+}
+#EC2 Instance
 resource "aws_instance" "TF_instance" {
   ami = "ami-0763cf792771fe1bd"
   instance_type = "t2.micro"
   key_name = "mykey100"
-  vpc_security_group_ids = ["sg-076de74cf330b6063"]
+  vpc_security_group_ids = aws_vpc.my_vpc.id
+  subnet_id = aws_subnet.my_subnet.id
+  security_groups = [ aws_security_group.my-sg.name ]
   tags = {
     name = "new_instance"
     env = "dev"
