dd45c1450cbea57085a62f394f72ae4c0283631b
Bhargav <karkarebhargav08@gmail.com>
2023-11-02 18:03:05 +0530

    vpc security group id fixed

--- Conf.tf
+++ Conf.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "TF_instance" {
   ami = "ami-0763cf792771fe1bd"
   instance_type = "t2.micro"
   key_name = "mykey100"
-  vpc_security_group_ids = "sg-076de74cf330b6063"
+  vpc_security_group_ids = "vpc-0358d2098d2b731a5"
   tags = {
     name = "new_instance"
     env = "dev"
